8ce3c9f619ff5429fa28077f18daa769fcbac2e2
Josh Miller <william.miller@att.net>
2019-12-30 11:51:56 -0600

    added .sol files

---
+++ AssociateProfitSplitter.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.5.0;
+
+// lvl 1: equal split
+contract AssociateProfitSplitter {
+    // @TODO: Create three payable addresses representing `employeeOne`, `employeeTwo` and `employeeThree`.
+    address payable employeeOne;
+    address payable employeeTwo;
+    address payable employeeThree;
+    
+    
+    constructor(address payable _one, address payable _two, address payable _three) public {
+        employeeOne = _one;
+        employeeTwo = _two;
+        employeeThree = _three;
+    }
+
+    function balance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function deposit() public payable {
+        // @TODO: Split `msg.value` into three
+        uint amount = msg.value / 3;
+
+        // @TODO: Transfer the amount to each employee
+        employeeOne.transfer(amount);
+        employeeTwo.transfer(amount);
+        employeeThree.transfer(amount);
+
+        // @TODO: take care of a potential remainder by sending back to HR (`msg.sender`)
+        uint leftoverAmount = msg.value - amount * 3;
+        msg.sender.transfer(leftoverAmount);
+    }
+
+    function() external payable {
+        // @TODO: Enforce that the `deposit` function is called in the fallback function!
+        deposit();
+    }
+}
